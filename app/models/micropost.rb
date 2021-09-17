e14a597c37bf0d69b31af801dbf4d1dbc7f07783
takashi.nagaoka <takashi.nagaoka@gmail.com>
2021-09-17 12:15:43 +0000

    clean routes etc

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -4,9 +4,7 @@ class Micropost < ApplicationRecord
   has_many :users ,through: :favorites,source: :user
   has_many :favorites
   has_many :favorings ,through: :favorites,source: :micropost
-  has_many :reverses_of_favorite ,class_name: 'Favorite',foreign_key: 'micropost_id'
-  has_many :favorited_users, through: :reverses_of_favorite,source: :user
-  
+
   validates :content, presence: true, length: { maximum: 255 }
 
   def favorites?(micropost)
--- config/routes.rb
+++ config/routes.rb
@@ -11,23 +11,9 @@ Rails.application.routes.draw do
     member do
       get :followings
       get :followers
-      get :favorings
-      get :favorited_users
       get :likes
     end
   end
-  
-#    member do
-#      get :favorings
-#      get :favorited_users
-#    end
-#  end
-  
-#  resources :users, only: [:index, :show, :new, :create] do
-#    member do
-#      get :likes
-#    end
-#  end
   
   resources :microposts, only: [:create, :destroy]
   resources :relationships, only: [:create, :destroy]
